47abc366ed2a6f7db9162832eccbf659354aff05
Ibrahima Sylla <syllaisa6@gmail.com>
2023-06-18 20:02:42 +0000

    add columns to table eleves

--- database/migrations/2023_06_18_163822_create_eleves_table.php
+++ database/migrations/2023_06_18_163822_create_eleves_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('eleves', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('type');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('address')->nullable();
+            $table->string('gender')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->binary('photo')->nullable();
             $table->timestamps();
         });
     }
